cd51c4205fb0c685caa5bc3a480d7e5c371df148
anbu177 <anbu.177@gmail.com>
2017-04-19 23:28:44 -0400

    Initial version

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 src = $(wildcard *.c)
 obj = $(src:.c=.o)
-CC = g++ -std=c++11 -g -Wall
-LDFLAGS = -ljson-c -lsqlite3
+CC = gcc -g -Wall -std=c99
+#LDFLAGS = -lboost_system -lboost_filesystem -lboost_thread -lpthread -lboost_regex -lcrypto -lssl -ljson-c -lsqlite3
+LDFLAGS = -ljson-c -lsqlite3 -lssl -lcrypto
 
 voc_client_app: $(obj)
 	$(CC) -o $@ $^ $(LDFLAGS)
 
 .PHONY: clean
 clean:
-	rm -f voc_client_app *~
+	rm -f voc_client_app *~ *.o
--- test_client.c
+++ test_client.c
@@ -1,11 +1,11 @@
-//#include "client.h"
-//#include "client_json.h"
-//#include "client_manifest.h"
-//#include "client_registration.h"
+#include "client.h"
+#include "client_json.h"
+#include "client_manifest.h"
+#include "client_registration.h"
 #include <json/json.h>
 #include <json/json_object.h>
 #include <stdio.h>
-//#include <string>
+#include <string.h>
 
 /*
 std::string make_request(std::string ip_address,  std::string schema_name,
@@ -116,6 +116,97 @@ int register_user(std::string arv1, std::string arv2, std::string arv3, std::str
   return 0;
 }
 */
+
+
+
+int register_user(char * arv1, char * arv2, char * arv3, char * arv4)
+{
+
+  char * path = "/Anaina/v0/Register";
+  eXtype type = eRegister;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+
+  // do the registration validation..
+  handle_registration_json(jstr, arv1);
+
+  return 0;
+}
+
+
+int get_manifest_from_server(char * arv1, char * arv2, char * arv3, char * arv4)
+{
+  char * path = "/Anaina/v0/Download-Manifest";
+  eXtype type = eGetManifest;
+
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+
+  printf("\n\njstr = %s\n\n",jstr);
+  printf("\n\njstr len = %ld\n\n",strlen(jstr));
+  if(strlen(jstr)){
+    install_cache(jstr);
+    system("test/generate_voc_html.py"); //Create output file Jon's python script
+  }
+
+  return 0;
+}
+
+
+int hello_voc(char * arv1, char * arv2, char * arv3, char * arv4)
+{
+  char * path = "/Anaina/v0/HelloVoC";
+  eXtype type = eHello;
+
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+
+  printf("json = %s\n",jstr);
+
+  return 0;
+}
+
+
+int get_provider_list(char * arv1, char * arv2, char * arv3, char * arv4){
+  char * path = "/Anaina/v0/Provider-List";
+  eXtype type = eProviderList;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+  printf("json = %s\n",jstr);
+  handle_provider_list(jstr, arv1);
+  return 0;
+}
+
+int get_topic_list(char * arv1, char * arv2, char * arv3, char * arv4){
+  char * path = "/Anaina/v0/topic_list";
+  eXtype type = eTopicList;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+  printf("json = %s\n",jstr);
+  return 0;
+}
+
+int get_profile(char * arv1, char * arv2, char *  arv3, char *  arv4){
+  char *  path = "/Anaina/v0/profile";
+  eXtype type = eProfile;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+  printf("json = %s\n",jstr);
+  return 0;
+}
+
+int get_policy(char * arv1, char * arv2, char * arv3, char * arv4){
+  char * path = "/Anaina/v0/getPolicy";
+  eXtype type = ePolicy;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+  printf("json = %s\n",jstr);
+  return 0;
+}
+
+
+int send_status(char * arv1, char * arv2, char * arv3, char * arv4)
+{
+  char * path = "/Anaina/v0/Status";
+  eXtype type = eStatus;
+  char * jstr = get_response(arv1, arv2, arv3, arv4, path, type);
+  printf("json = %s\n",jstr);
+  return 0;
+}
+
 int main(int argc, char* argv[])
 {
   //try{
@@ -125,16 +216,32 @@ int main(int argc, char* argv[])
 	printf( "Usage: client <server> <schemaName> <tenantId> <publicKey>\n");
 	return 1;
       }
-    /*
-    std::string user = validate_user_for_registration();
-    if(user.empty())
+    printf("Validating the user\n");
+    char * user = validate_user_for_registration();
+    printf("User - %s\n",user);
+
+    if(strlen(user))
       register_user(argv[1], argv[2], argv[3], argv[4]);
     else
-      std::cout << "User is previously registered " << std::endl;
-    */
+      printf("User is previously registered\n");
+
     //}catch (std::exception& e){
     //std::cerr << "Caught input Exception: " << e.what() << "\n";
     ///}
+
+
+    printf("\n\nRequesting for manifest..\n\n");
+
+    get_manifest_from_server(argv[1], argv[2], argv[3], argv[4]);
+    //hello_voc(argv[1], argv[2], argv[3], argv[4]);
+    //get_provider_list(argv[1], argv[2], argv[3], argv[4]);
+    //get_topic_list(argv[1], argv[2], argv[3], argv[4]);
+    //get_profile(argv[1], argv[2], argv[3], argv[4]);
+    //get_policy(argv[1], argv[2], argv[3], argv[4]);
+    send_status(argv[1], argv[2], argv[3], argv[4]);
+
+    return 1;
+
 
  while(1)
    {
@@ -172,5 +279,3 @@ int main(int argc, char* argv[])
    }
  return 0;
 }
-
-
